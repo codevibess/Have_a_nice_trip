dfbf9b79d0c487e5b31f7e0d379a7bbf027a713b
Andriy Cherevko <cherevkin@gmail.com>
2018-08-04 13:17:48 +0200

    added user input messeg

--- HaveANiceTripBot.py
+++ HaveANiceTripBot.py
@@ -4,7 +4,9 @@ from telegram.ext import MessageHandler
 
 from kiwi_controller import *
 import logging
+
 questions = ['city', 'city_to', 'data_f', 'data_t', 'passengers']
+values = []
 # from config import *
 
 #  log into console - very helpful  stuff
@@ -16,31 +18,32 @@ updater = Updater(TELEGRAM_TOKEN)
 # e.t.c . It runs asynchronously in a separate thread.
 job_q = updater.job_queue
 
+
 def give_a_questions():
     yield from questions
 
+
 q = give_a_questions()
 
-def search(bot,job):
+
+def search(bot, update):
     global q
     try:
         question = next(q)
+        values.append(update.message.text)
     except:
         q = give_a_questions()
+        print("-------------------" + str(values))
         return
     bot.send_message(chat_id=CHAT_ID,
                      text=question)
-
-
-
-
 
 
 def send_updates_for_users(bot, job):
     global x
     couter = 25
     prev_counter = 0
-    for it in range(0, int(len(x)/couter)+1):
+    for it in range(0, int(len(x) / couter) + 1):
         if len(x) >= 25:
             bot.send_message(parse_mode='HTML', chat_id=CHAT_ID,
                              text=f'''{''.join(x[prev_counter:couter])}''')
@@ -51,9 +54,8 @@ def send_updates_for_users(bot, job):
                              text=x)
 
 
-
 handler = MessageHandler(Filters.text | Filters.command, search)
-# updater.dispatcher.add_handler(handler)  # ставим обработчик всех текстовых сообщений
+updater.dispatcher.add_handler(handler)  # ставим обработчик всех текстовых сообщений
 updater.dispatcher.add_handler(CommandHandler('search', search))
 
 my_updates_sender = job_q.run_repeating(search, interval=90, first=0)
